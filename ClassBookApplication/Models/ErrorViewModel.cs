d2de9c35b73744dcb62c4dad8fa99b2dbf374f3c
Rakesh Girase <rakeshgirase6008@gmail.com>
2020-08-21 19:54:55 +0530

    => Add the Simple MVC Application

---
+++ ClassBookApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClassBookApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
